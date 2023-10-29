64808de3f016670d8f1b2b9b016ca23192c58784
Crowell621 <146191682+Crowell621@users.noreply.github.com>
2023-10-29 21:46:19 +0800

    progress ivan rusak

---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+
+// export function Footer() {
+//   return <footer></footer>;
+// }
+import React from 'react';
+import './components/footer_style.css'; // Relative path to your CSS file
+
+function Footer() {
+  return (
+    <footer>
+      This is your footer.
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ src/components/main.tsx
@@ -0,0 +1,3 @@
+export function Main({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>;
+}
