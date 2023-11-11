a5e4a40e891f014574ebe9c387ea66d4c3937a2d
Crowell621 <144347126+Chromewell621@users.noreply.github.com>
2023-11-12 00:35:26 +0800

    dropdown button dan search bar
    yang sudah fix baru dropdown button

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Tomboldiskusi } from "@/components/ui/tomboldiskusi"
+import React, { useState, useEffect} from 'react'
 export default function Forum() {
-  return <div className="justify-start flex py-10">
-    <section className="pl-60 pt-10">
+  return <div className="justify-start flex py-6">
+    <section className="pl-60">
       <div className="container-md max-w-none text-center">
         <Tomboldiskusi text={"Buat Diskusi"}/>
       </div>
@@ -84,9 +86,44 @@ export default function Forum() {
         </nav>
       </div>
     </section>
-    <section className="pt-10">
-      <div className="pl-10">
-        <p>place holder search bar</p>
+
+
+    <section className="mx-40 flex">
+      <div>       
+        <form>   
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"> 
+                  </div>
+                <input type="search" id="default-search" className="block w-full p-4 pr-28 text-sm  text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-zinc-500 focus:border-zinc-500 " placeholder="Search..." required/>
+                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-black hover:bg-zinc-500 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+            </div>
+        </form>
+      </div>
+
+      <div className="px-32">
+        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-black hover:bg-zinc-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Dropdown button <svg name="w-1 h-1 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
+        </button>
+
+          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 ">
+              <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownDefaultButton">
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Dashboard</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Settings</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Earnings</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Sign out</a>
+                </li>
+              </ul>
+          </div>
+
       </div>
     </section>
   </div>
